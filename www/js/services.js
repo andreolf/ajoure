5b653e3f31fdd344228bcb1899b01f6ac979c0c3
F-A <andreolf@francescos-MacBook-Air.local>
2015-10-17 18:54:46 -0400

    test4

--- www/js/services.js
+++ www/js/services.js
@@ -6,27 +6,27 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'Milk',
     lastText: 'You on your way?',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: 'Eggs',
     lastText: 'Hey, it\'s me',
     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Pepsi',
     lastText: 'I should buy a boat',
     face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Doritos',
     lastText: 'Look at my mukluks!',
     face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Salats',
     lastText: 'This is wicked good ice cream.',
     face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
   }];
