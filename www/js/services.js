e0446ab317fdca1e4a67a3d8f719061ed465cfda
F-A <andreolf@francescos-MacBook-Air.local>
2015-10-17 22:36:19 -0400

    test7

--- www/js/services.js
+++ www/js/services.js
@@ -33,9 +33,33 @@ angular.module('starter.services', [])
 
 
 
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+
+.factory('Refs', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
   var refs = [{
     id: 0,
-    name: 'Milk1111',
+    name: 'Milk',
     lastText: 'You on your way?',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
@@ -62,20 +86,81 @@ angular.module('starter.services', [])
 
 
 
+
   return {
     all: function() {
-      return chats;
+      return refs;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(ref) {
+      refs.splice(refs.indexOf(ref), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(refId) {
+      for (var i = 0; i < refs.length; i++) {
+        if (refs[i].id === parseInt(refId)) {
+          return refs[i];
         }
       }
       return null;
     }
   };
-});
+})
+
+
+
+.factory('bChats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var bchats = [{
+    id: 0,
+    name: 'Milk',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Eggs',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Pepsi',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Doritos',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Salats',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+
+
+
+  return {
+    all: function() {
+      return bchats;
+    },
+    remove: function(bchat) {
+      bchats.splice(bchats.indexOf(bchat), 1);
+    },
+    get: function(bchatId) {
+      for (var i = 0; i < bchats.length; i++) {
+        if (bchats[i].id === parseInt(bchatId)) {
+          return bchats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+;
+
+
+
+
